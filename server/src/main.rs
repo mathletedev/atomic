858017102349c31b0dfe2b64980472211631b24a
Neal Wang <nealwang.sh@protonmail.com>
2023-10-07 01:30:35 -0700

    server + web init

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,9 +1,20 @@
 use std::net::SocketAddr;
 
 use axum::{routing::get, Router};
+use sqlx::postgres::PgPoolOptions;
+
+async fn root() -> String {
+	String::from("Hello, world!")
+}
 
 #[tokio::main]
 async fn main() {
+	let pool = PgPoolOptions::new()
+		.max_connections(4)
+		.connect("postgres://user:password@localhost/atomic")
+		.await
+		.unwrap();
+
 	let app = Router::new().route("/", get(root));
 
 	let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
@@ -11,8 +22,4 @@ async fn main() {
 		.serve(app.into_make_service())
 		.await
 		.unwrap();
-}
-
-async fn root() -> &'static str {
-	"Hello, world!"
 }
